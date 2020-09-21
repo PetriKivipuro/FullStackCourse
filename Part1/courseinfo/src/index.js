e8e3b408ebb8c0f9c8e77ced03eba418dfcc2341
Petri <petri.kivipuro@gmail.com>
2020-09-21 14:16:19 +0300

    courseinfo 1.1 and 1.2 push

---
+++ Part1/courseinfo/src/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <div>
+      <p>{props.course}</p>
+    </div>
+  )
+}
+const Content = (props) => {
+
+  return (
+    <div>
+      <p>{props.part}, {props.exercises} </p>
+    </div>
+  )
+}
+const Total = (props) => {
+  return (
+    <div>
+      <p>Total amount of exercises: {props.exercises}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <h1>
+        <Header course={course} />
+      </h1>
+      <span>
+        <Content part={part1} exercises={exercises1} />
+        <Content part={part2} exercises={exercises2} />
+        <Content part={part3} exercises={exercises3} />
+
+        <Total exercises={exercises1 + exercises2 + exercises3} />
+      </span>
+    </div>
+  )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
