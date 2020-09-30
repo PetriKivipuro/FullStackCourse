9c498556df483e4f83f17ac7613f7c0fd10de302
Petri Kivipuro SOFTK20P <petri.kivipuro@centria.fi>
2020-09-30 17:07:35 +0300

    test

--- Part1/unicafe/src/index.js
+++ Part1/unicafe/src/index.js
@@ -1,13 +1,26 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = (props) => {
+console.log('props good amount tähän', props.good)
+
+return(
+<div>
+<h2>Statistics</h2>
+ 
+ good: {props.good}
+
+
+
+</div>
+)
+}
 
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
 
   const kasvataYkshyva = () => setGood(good + 1)
   const kasvataYksnetrl = () => setNeutral(neutral + 1)
@@ -33,14 +46,13 @@ const App = () => {
         bad
       </button>
 
-      <h2>Statistics</h2>
+      <Statistics/>
 
-      <div>good: {good}</div>
-      <div>neutral: {neutral}</div>
-      <div>bad: {bad}</div>
-      <div>all: {total} </div>
-      <div>average: {(good-bad)/(total)} </div>
-      <div>positive: {good/(total)*100} %</div>
+     <div>neutral: {neutral}</div>
+<div>bad: {bad}</div>
+<div>all: {total} </div>
+<div>average: {(good-bad)/(total)} </div>
+<div>positive: {good/(total)*100} %</div>
 
 
 
