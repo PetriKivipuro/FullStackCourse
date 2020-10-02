4a7cd5c9e962e83c8acf3ade1f8b84eb5f8717d8
Petri Kivipuro SOFTK20P <petri.kivipuro@centria.fi>
2020-10-02 11:25:48 +0300

    1.9d

--- Part1/unicafe/src/index.js
+++ Part1/unicafe/src/index.js
@@ -3,18 +3,28 @@ import ReactDOM from 'react-dom'
 
 const Statistics = (props) => {
 const total = props.good+props.bad+props.neutral
-console.log('props good amount tähän', props.good)
+
+
+if( total ===0){
+return(
+<div>
+<h1>Statistics</h1>
+
+<p>No feedback givennnn</p>
+</div>
+)
+}
 
 return(
 <div>
-<h2>Statistics</h2>
+<h1>Statistics</h1>
  
   <div>good: {props.good}</div>
   <div>neutral: {props.neutral}</div>
- <div> bad: {props.bad}</div>
+  <div> bad: {props.bad}</div>
   <div>all: {total}</div>
   <div>average: {(props.good-props.bad)/(total)} </div>
- <div> positive: {props.good/(total)*100} %</div>
+  <div> positive: {props.good/(total)*100} %</div>
 
 </div>
 )
@@ -25,15 +35,14 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const [noFeedback] = useState([])
+  
   const kasvataYkshyva = () => setGood(good + 1)
   const kasvataYksnetrl = () => setNeutral(neutral + 1)
   const kasvataYksbad = () => setBad(bad + 1)
   console.log('good', good,'neutra', neutral,'bad', bad)
   
   
-
-
   return (
     <div>
       <h1>Give feedback</h1>
@@ -50,7 +59,7 @@ const App = () => {
         bad
       </button>
 
-      <Statistics good={good} neutral= {neutral} bad={bad} />
+      <Statistics good={good} neutral= {neutral} bad={bad} noFeedback={noFeedback}/>
 
      </div>
   )
