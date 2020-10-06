2edc57a93639884a132d696e6a89a00151dc6697
Petri <petri.kivipuro@gmail.com>
2020-10-06 19:12:55 +0300

    2.1change to header

--- Part2/courseinfo/src/index.js
+++ Part2/courseinfo/src/index.js
@@ -6,8 +6,8 @@ const Content = (props) => {
   console.log('tehtävämääränkunsaistähän: ', props.course.parts[0].exercises)
   return (
     <div>
-      <h1>Half Stack application development</h1>
 
+      <h1>{props.course.name}</h1>
       <p>{props.course.parts[0].name},  {props.course.parts[0].exercises}</p>
       <p>{props.course.parts[1].name}, {props.course.parts[1].exercises}</p>
       <p>{props.course.parts[2].name}, {props.course.parts[2].exercises}</p>
