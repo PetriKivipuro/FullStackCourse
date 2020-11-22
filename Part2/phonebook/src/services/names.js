cc1a3938158d67db55e5ac60362ff2d8f3a0ef7f
Petri Kivipuro SOFTK20P <petri.kivipuro@centria.fi>
2020-11-22 18:39:17 +0200

    2.16 done

--- Part2/phonebook/src/services/names.js
+++ Part2/phonebook/src/services/names.js
@@ -10,8 +10,12 @@ const create = newObject => {
     return axios.post(baseUrl, newObject)
 }
 
+export default {getAll, create}
+
+/* 
 const update = (id, newObject) => {
     return axios.put(`${baseUrl}/${id}`, newObject)
 }
-
 export default {getAll, create, update}
+
+*/
